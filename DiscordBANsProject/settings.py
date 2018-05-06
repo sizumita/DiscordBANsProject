0bcef2043dafe8e485ce3248c1b67b2382fa4da9
sizumita <sumito@izumita.com>
2018-05-06 15:28:43 +0900

    others

--- DiscordBANsProject/settings.py
+++ DiscordBANsProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["discordbans.net"]
+ALLOWED_HOSTS = ["discordbans.net", "0.0.0.0"]
 
 
 # Application definition
