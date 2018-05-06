b3bae046ac3b647dfbeceb5752cb726bf01c3edb
sizumita <sumito@izumita.com>
2018-05-06 15:25:17 +0900

    urlの設定 indexページの作成

--- DiscordBANsProject/settings.py
+++ DiscordBANsProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["discordbans.net"]
 
 
 # Application definition
